5458a9e863b20e07dc40dabfe7e6c369275fa784
entropy88 <36485631+entropy88@users.noreply.github.com>
2026-02-10 15:32:29 +0200

    Add files via upload

--- extractor.js
+++ extractor.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("bibtexFile");
   const output = document.getElementById("output");
-  const records_count = document.getElementById("records_count")
+  const records_count = document.getElementById("records_count");
+
+
+
+
+
 
   //on file selected load content
   input.addEventListener("change", () => {
+
+
     const file = input.files[0];
     if (!file) return;
 
@@ -12,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     reader.onload = () => {
       // Display raw text safely
+
       formISBD(reader.result)
 
     };
@@ -25,10 +33,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //get data and parse it
-//so far it only displays raw
+
 function formISBD(bibtexData) {
-
-
 
   //get the array of objects
   let records = bibtexRawToObjectsArray(bibtexData);
@@ -37,27 +43,88 @@ function formISBD(bibtexData) {
   records_count.textContent = `${records.length} източника`
 
 
-  //display the records as text. first we do the articles
-  let bibliography = "";
+
+
   records.forEach(r => {
-    //check record type and append value
-    bibliography += checkRecordType(r);
-    
+    //check record type and push value to the corresponding array
+    checkRecordType(r);
+
+
   })
+
 }
+
+
 
 //check if the record is an article or other, based on used pages field, returns a literal
 function checkRecordType(record) {
+
+  //create arrays for records
+  //in order to sort records we need 2 arrays, ones for books, and one for articles
+  const books_array = [];
+  const articles_array = [];
+
+  //deciding if a record is a book or not based on field pages_art
   if (record.pages_art.length > 0) {
     record.record_type = "Статия"
-    articleIsbd(record);
-    return "Статия"
+    //push record to articles_array
+    articles_array.push(record)
+    console.log('record pushed to articles')
+
+    //this needs to happen after
+    // articleIsbd(record);
+
   } else {
     record.record_type = "Книга"
-    bookIsbd(record);
-    return "Книга"
+    //push record to book_arrays
+    books_array.push(record)
+    console.log('record pushed to books')
+
+    //this needs to happen later
+    // bookIsbd(record);
+
   }
+
+  //where the hell do I put you???
+
+  sort_records(books_array);
+  sort_records(articles_array);
+
+  console.log(books_array);
+  console.log(articles_array)
+
+   //create label
+  let book_label=document.createElement('p');
+  book_label.textContent="Книги";
+  output.appendChild(book_label);
+
+  
+  //form and display
+  books_array.forEach(r => {
+    bookIsbd(r)
+  })
+
+    //create label
+  let articles_label=document.createElement('p');
+  book_label.textContent="Статии";
+  output.appendChild(articles_label);
+
+  articles_array.forEach(r => {
+    articleIsbd(r)
+  })
+
+  //arrays should be populated and sorted
+
 }
+
+
+  //function to sort records by sortwort, currently author in the formed arrays
+  function sort_records(array) {
+    return array.sort((a, b) =>
+      a.author.localeCompare(b.author, undefined, { sensitivity: "base" })
+    );
+  }
+
 
 function bookIsbd(record) {
   console.log('trigegr book isbd')
@@ -65,40 +132,40 @@ function bookIsbd(record) {
   let recordP = document.createElement('p');
   let recordContent = record.record_type;
 
-  recordContent+='\n'
+  recordContent += '\n'
   //signature
-  recordContent+=`${record.signature}\n`
+  recordContent += `${record.signature}\n`
   //title
-  recordContent+=record.title;
+  recordContent += record.title;
   //authors
-  if (record.author.length>0){
-    recordContent+="/";
-    record.author.forEach(a=>{
-      recordContent+=` ${a}; `
+  if (record.author.length > 0) {
+    recordContent += "/";
+    record.author.forEach(a => {
+      recordContent += ` ${a}; `
     })
   }
   //add place
-   if (record.address.length>0){
-    recordContent+=`. ${record.address}`
-   }
-   //add publisher
-   if (record.publisher.length>0){
-    recordContent+=`: ${record.publisher}`
-   }
-   //add year
-   if (record.year.length>0){
-    recordContent+=`, ${record. year}`
-   }
-   //add pages
-   if (record.pages.length>0){
-    recordContent+= ` . - ${record.pages[0]}`
-   }
-   //add illustrations
-   if (record.illustrations.length>0){
-    recordContent+=` : ${record.illustrations}`
-   }
-  
-  recordP.textContent=recordContent;
+  if (record.address.length > 0) {
+    recordContent += `. ${record.address}`
+  }
+  //add publisher
+  if (record.publisher.length > 0) {
+    recordContent += `: ${record.publisher}`
+  }
+  //add year
+  if (record.year.length > 0) {
+    recordContent += `, ${record.year}`
+  }
+  //add pages
+  if (record.pages.length > 0) {
+    recordContent += ` . - ${record.pages[0]}`
+  }
+  //add illustrations
+  if (record.illustrations.length > 0) {
+    recordContent += ` : ${record.illustrations}`
+  }
+
+  recordP.textContent = recordContent;
   output.appendChild(recordP)
 }
 
@@ -122,11 +189,11 @@ function articleIsbd(record) {
   recordContent += `. - бр. ${record.issue}`
   recordContent += ` (${record.year})`
   recordContent += `, с. ${record.pages_art}`
-   //add illustrations
-   if (record.illustrations.length>0){
-    recordContent+=` : ${record.illustrations}`
-   }
-  recordContent+='\n'
+  //add illustrations
+  if (record.illustrations.length > 0) {
+    recordContent += ` : ${record.illustrations}`
+  }
+  recordContent += '\n'
 
   //next 2 cases check array length becaause there can be multiple values
   // if there is abstract
@@ -176,7 +243,7 @@ function bibtexRawToObjectsArray(bibtexData) {
     "issue",
     "abstract",
     "see_also",
-    "address", 
+    "address",
     "publisher",
     "illustrations",
     "signature"
@@ -204,3 +271,4 @@ function bibtexRawToObjectsArray(bibtexData) {
     return obj;
   });
 }
+
